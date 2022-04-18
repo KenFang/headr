f8784e31a612cdecc00723004cc2decb7e0cbca6
KenFang <kenfangch@yahoo.com.tw>
2022-04-19 06:43:48 +0800

    Add files via upload

---
+++ src/lib.rs
@@ -0,0 +1,93 @@
+use clap::{App, Arg};
+use std::error::Error;
+use std::fmt::format;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("headr")
+        .version("0.1.0")
+        .author("Ken Fang <kenfang@deva9.com>")
+        .about("Rust head")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of lines")
+                .default_value("10")
+                .conflicts_with("bytes"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .help("Number of bytes")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let lines = matches
+        .values_of("lines")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal bytes count -- {}", e))?;
+
+
+    let bytes = matches
+        .values_of("bytes")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal bytes count -- {}", e))?;
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        lines: lines.unwrap(),
+        bytes,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    match val.parse() {
+        Ok(n) if n > 0 => Ok(n),
+        _ => Err(From::from(val)),
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    // 3 is and OK integer
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    // Any string is an error
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    // A zero is an error
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
